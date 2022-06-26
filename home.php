e83e987f0da4e93bbbb8b6fa0b24389ffbe8518c
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-26 13:14:17 +0800

    commit

--- home.php
+++ home.php
@@ -1,66 +1,9 @@
-<?php 
-include('functions.php');
-
-if (!isAdmin()) {
-	$_SESSION['msg'] = "You must log in first";
-	header('location: login.php');
-}
-
-if (isset($_GET['logout'])) {
-	session_destroy();
-	unset($_SESSION['user']);
-	header("location: login.php");
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="../style.css">
-	<style>
-	.header {
-		background: #003366;
-	}
-	button[name=register_btn] {
-		background: #003366;
-	}
-	</style>
-</head>
-<body>
-	<div class="header">
-		<h2>Admin - Home Page</h2>
-	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
-
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="../images/admin_profile.png"  >
-
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="home.php?logout='1'" style="color: red;">logout</a>
-                       &nbsp; <a href="create_user.php"> + add user</a>
-					</small>
-
-				<?php endif ?>
-			</div>
-		</div>
-	</div>
-</body>
+<?php session_start(); ?><html>
+   <head>
+      <title>Welcome </title>
+   </head>
+   <body>
+      <h1>Welcome <?php echo $_SESSION['username']; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
 </html>
